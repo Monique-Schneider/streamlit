f4d89fa6eac4c124861e3a6998725db566362606
Monique-Schneider <83307613+Monique-Schneider@users.noreply.github.com>
2021-11-08 15:32:09 -0300

    Update real.py

--- real.py
+++ real.py
@@ -11,39 +11,42 @@ kpi1, kpi2 = st.beta_columns(2)
 
 kinesis_client = boto3.client('kinesis', aws_access_key_id=id, aws_secret_access_key=key)
 
-my_stream_name = 'ExampleInputStream'
+if (id!=0 & key!=0):
+    my_stream_name = 'ExampleInputStream'
 
-response = kinesis_client.describe_stream(StreamName=my_stream_name)
-my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+    response = kinesis_client.describe_stream(StreamName=my_stream_name)
+    my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
 
-shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name, ShardId=my_shard_id, ShardIteratorType='LATEST')
+    shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name, ShardId=my_shard_id, ShardIteratorType='LATEST')
 
-my_shard_iterator = shard_iterator['ShardIterator']
-record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator,Limit=100)
+    my_shard_iterator = shard_iterator['ShardIterator']
+    record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator,Limit=100)
 
-while 'NextShardIterator' in record_response:
-    record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],Limit=1)
+    while 'NextShardIterator' in record_response:
+        record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'],Limit=1)
 
-    if(record_response['Records']):
-        #file_content = obj.get()['Body'].read()
-        #df = pd.read_json(file_content, lines=True)
-        a = record_response['Records'][0]['Data']
-        dict_str = a.decode("UTF-8")
-        mydata = ast.literal_eval(dict_str)
-        x = mydata.get('x')
-        y = mydata.get('y')
-        with kpi1:
-            st.markdown('**X**')
-            number1 = x
-            st.markdown(f"<h1 style='text-align: center; color: red;'>{x}</h1>", unsafe_allow_html=True)
+        if(record_response['Records']):
+            #file_content = obj.get()['Body'].read()
+            #df = pd.read_json(file_content, lines=True)
+            a = record_response['Records'][0]['Data']
+            dict_str = a.decode("UTF-8")
+            mydata = ast.literal_eval(dict_str)
+            x = mydata.get('x')
+            y = mydata.get('y')
+            with kpi1:
+                st.markdown('**X**')
+                number1 = x
+                st.markdown(f"<h1 style='text-align: center; color: red;'>{x}</h1>", unsafe_allow_html=True)
 
-        with kpi2:
-            st.markdown('**Y**')
-            number2 = y
-            st.markdown(f"<h1 style='text-align: center; color: red;'>{y}</h1>", unsafe_allow_html=True)
+            with kpi2:
+                st.markdown('**Y**')
+                number2 = y
+                st.markdown(f"<h1 style='text-align: center; color: red;'>{y}</h1>", unsafe_allow_html=True)
 
 
-  
-    time.sleep(1)
+    
+        time.sleep(1)
+
+ 
 
  
