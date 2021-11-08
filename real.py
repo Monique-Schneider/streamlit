a6f94f6c83e5f6d576de45cd371f56c8a4b0fe3c
Monique-Schneider <83307613+Monique-Schneider@users.noreply.github.com>
2021-11-08 15:47:29 -0300

    Update real.py

--- real.py
+++ real.py
@@ -12,12 +12,12 @@ kpi1, kpi2 = st.beta_columns(2)
 with kpi1:
     st.markdown('**X**')
     #number1 = 0
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{x}</h1>", unsafe_allow_html=True)
+    
 
 with kpi2:
     st.markdown('**Y**')
     #number2 = 0
-    st.markdown(f"<h1 style='text-align: center; color: red;'>{y}</h1>", unsafe_allow_html=True)
+    
 
 if (id!=0 and key!=0):
     kinesis_client = boto3.client('kinesis', aws_access_key_id=id, aws_secret_access_key=key, region_name='us-east-1')
@@ -42,9 +42,9 @@ if (id!=0 and key!=0):
             mydata = ast.literal_eval(dict_str)
             x = mydata.get('x')
             y = mydata.get('y')
-            number1 = x
-            number2 = y
-
+   
+            st.markdown(f"<h1 style='text-align: center; color: red;'>{x}</h1>", unsafe_allow_html=True)
+            st.markdown(f"<h1 style='text-align: center; color: red;'>{y}</h1>", unsafe_allow_html=True)
 
     
         time.sleep(20)
